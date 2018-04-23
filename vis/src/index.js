c441fa35a1f5cbf1d029280aa68ff98b7a45d707
Roni Choudhury <roni.choudhury@kitware.com>
2018-04-23 15:56:40 -0400

    Redo web app to take multiple, dynamically created vis

--- vis/src/index.js
+++ vis/src/index.js
@@ -1,8 +1,27 @@
-function wait () {
-  return Promise.resolve(47);
+function newDiv (id) {
+  const div = document.createElement('div')
+  div.setAttribute('id', id);
+
+  document.body.appendChild(div);
+  return div;
+}
+
+async function getData () {
+  const req = await fetch('diva.json');
+  return await req.json();
 }
 
 (async function () {
-  const value = await wait();
-  console.log(value);
+  const data = await getData();
+
+  const el = newDiv('vis');
+  const vis = new candela.components.ScatterPlot(el, {
+    data,
+    x: 'Frames',
+    y: 'Annotation Time (/spend)',
+    color: 'Annotator',
+    width: 960,
+    height: 540
+  });
+  vis.render();
 }());
