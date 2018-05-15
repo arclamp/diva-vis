e6e15639296af1ef994895153f33b3aacb87a5bd
Roni Choudhury <roni.choudhury@kitware.com>
2018-05-15 15:41:39 -0400

    WIP

---
+++ vis/src/BoxPlot.js
@@ -0,0 +1,14 @@
+import { VisComponent } from '@candela/core';
+
+function boxplot_prep (data, field) {
+
+}
+
+class BoxPlot extends VisComponent {
+  constructor (el, options) {
+    super(el);
+
+
+
+  }
+}
